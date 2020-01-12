9af62be8ec066e20e1ec7740f52b19b0857b58e0
Mochamad Lucky Pradana <luckypradana123@yandex.ua>
2020-01-12 16:05:35 +0700

    add styling?

--- src/hilih_lang/core.cljs
+++ src/hilih_lang/core.cljs
@@ -12,21 +12,30 @@
   (str/replace text #"[aueo]" "i"))
 
 ;; layouts
-(defn hello-world []
-  [:div
-   [:h2 (convert-to-hilih "Convert words to Hilih Language")]
-   [:textarea
-    {:rows 10
-      :placeholder "Please input words to be converted to hilih lang..."
-     :on-change   #(swap! app-state assoc-in [:text] (-> % .-target .-value))}]
-   [:h3 (convert-to-hilih (:text @app-state))]])
+(defn section []
+  (fn []
+    (let [text (:text @app-state)]
+      [:div.section
+       [:h1.section__title (convert-to-hilih "Convert words to Hilih Language")]
+       [:textarea
+        {:rows          10
+         :default-value (:text @app-state)
+         :placeholder   "Please input words to be converted to hilih lang..."
+         :on-change     #(swap! app-state assoc-in [:text] (-> % .-target .-value))}]
+       [:h3
+        (if (= text "")
+          ""
+          (convert-to-hilih "Result :"))
+        [:br]
+        [:br]
+        (convert-to-hilih text)]])))
 
 ;; setup
 (defn get-app-element []
   (gdom/getElement "app"))
 
 (defn mount [el]
-  (reagent/render-component [hello-world] el))
+  (reagent/render-component [section] el))
 
 (defn mount-app-element []
   (when-let [el (get-app-element)]
