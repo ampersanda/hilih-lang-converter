ddba8c56bb1fc8d5a2b1872ad749b6f00dfe3166
Mochamad Lucky Pradana <luckypradana123@yandex.ua>
2020-01-12 15:38:37 +0700

    initial project

---
+++ src/hilih_lang/core.cljs
@@ -0,0 +1,37 @@
+(ns ^:figwheel-hooks hilih-lang.core
+  (:require
+    [goog.dom :as gdom]
+    [clojure.string :as str]
+    [reagent.core :as reagent
+     :refer           [atom]]))
+
+;; state
+(defonce app-state (atom {:text ""}))
+
+(defn convert-to-hilih [text]
+  (str/replace text #"[aiueo]" "i"))
+
+;; layouts
+(defn hello-world []
+  [:div
+   [:textarea
+    {:rows 10
+      :placeholder "Please input words to be converted to hilih lang..."
+     :on-change   #(swap! app-state assoc-in [:text] (-> % .-target .-value))}]
+   [:h3 (convert-to-hilih (:text @app-state))]])
+
+;; setup
+(defn get-app-element []
+  (gdom/getElement "app"))
+
+(defn mount [el]
+  (reagent/render-component [hello-world] el))
+
+(defn mount-app-element []
+  (when-let [el (get-app-element)]
+    (mount el)))
+
+(mount-app-element)
+
+(defn ^:after-load on-reload []
+  (mount-app-element))
---
+++ test/hilih_lang/core_test.cljs
@@ -0,0 +1,3 @@
+(ns hilih-lang.core-test
+    (:require
+     [cljs.test :refer-macros [deftest is testing]]))
